bb7c6cd4a58219afaa4d763369c1c0b8349a48ec
Ahmed V2 <95228377+AhmedakV2@users.noreply.github.com>
2026-08-07 17:12:24 +0300

    V26.02

--- Aft/src/preload/index.ts
+++ Aft/src/preload/index.ts
@@ -4,6 +4,9 @@ const api = {
   execute: (action: unknown) => ipcRenderer.invoke('aft:execute', action),
   setVision: (on: boolean) => ipcRenderer.invoke('aft:vision', on),
   home: () => ipcRenderer.invoke('aft:home'),
+  back: () => ipcRenderer.invoke('aft:back'),
+  forward: () => ipcRenderer.invoke('aft:forward'),
+  reload: () => ipcRenderer.invoke('aft:reload'),
   versions: process.versions
 }
 
